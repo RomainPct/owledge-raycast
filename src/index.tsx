e556a41e4013bf0c3e51236974243c0e99c7527e
Romain Penchenat <romain.penchenat@gmail.com>
2024-04-15 22:21:22 +0200

    ✨ Raycast extension v1

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,39 @@
-import { ActionPanel, Action, List } from "@raycast/api";
-import { useFetch, Response } from "@raycast/utils";
+import { ActionPanel, Action, List, getPreferenceValues } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
 import { useState } from "react";
 import { URLSearchParams } from "node:url";
 
+interface Preferences {
+  WORKSPACE_ID: string;
+  API_KEY: string;
+}
+
 export default function Command() {
+  const preferences = getPreferenceValues<Preferences>();
   const [searchText, setSearchText] = useState("");
   const { data, isLoading } = useFetch(
-    "https://api.npms.io/v2/search?" +
-      // send the search query to the API
-      new URLSearchParams({ q: searchText.length === 0 ? "@raycast/api" : searchText }),
+    `https://main.api.owledge.app/v1/raycast_search/${preferences.WORKSPACE_ID}?` +
+      new URLSearchParams({ query: searchText }).toString(),
     {
+      headers: {
+        Authorization: `Bearer ${preferences.API_KEY}`,
+      },
       parseResponse: parseFetchResponse,
-    }
+    },
   );
 
   return (
-    <List
-      isLoading={isLoading}
-      onSearchTextChange={setSearchText}
-      searchBarPlaceholder="Search npm packages..."
-      throttle
-    >
-      <List.Section title="Results" subtitle={data?.length + ""}>
-        {data?.map((searchResult) => (
-          <SearchListItem key={searchResult.name} searchResult={searchResult} />
-        ))}
-      </List.Section>
+    <List isLoading={isLoading} onSearchTextChange={setSearchText} searchBarPlaceholder="Search in Owledge..." throttle>
+      {data?.length == 0 ? (
+        <List.EmptyView
+          icon={{ source: "http://search.owledge.app/images/logo.svg" }}
+          title={`No result found for "${searchText}"`}
+        />
+      ) : (
+        <List.Section title="Owledge results" subtitle={data?.length + ""}>
+          {data?.map((searchResult) => <SearchListItem key={searchResult.uid} searchResult={searchResult} />)}
+        </List.Section>
+      )}
     </List>
   );
 }
@@ -33,19 +41,35 @@ export default function Command() {
 function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
   return (
     <List.Item
-      title={searchResult.name}
-      subtitle={searchResult.description}
-      accessories={[{ text: searchResult.username }]}
+      title={searchResult.title}
+      subtitle={searchResult.subtitle}
+      icon={`https://search.owledge.app/images/${searchResult.icon}`}
+      accessories={[
+        {
+          text: `Last edit: ${searchResult.last_edited_at}`,
+        },
+      ]}
       actions={
         <ActionPanel>
           <ActionPanel.Section>
-            <Action.OpenInBrowser title="Open in Browser" url={searchResult.url} />
+            <Action.OpenInBrowser title="Open" url={searchResult.url} />
+            <Action.OpenInBrowser title={`Open parent "${searchResult.parent_path}"`} url={searchResult.parent_url} />
+            <Action.OpenInBrowser
+              shortcut={{ modifiers: ["opt"], key: "enter" }}
+              title="Search in Owledge app"
+              url={searchResult.owledge_search_url}
+            />
           </ActionPanel.Section>
           <ActionPanel.Section>
             <Action.CopyToClipboard
-              title="Copy Install Command"
-              content={`npm install ${searchResult.name}`}
+              title="Copy url"
+              content={searchResult.url}
               shortcut={{ modifiers: ["cmd"], key: "." }}
+            />
+            <Action.CopyToClipboard
+              title="Copy parent url"
+              content={searchResult.parent_url}
+              shortcut={{ modifiers: ["cmd"], key: ";" }}
             />
           </ActionPanel.Section>
         </ActionPanel>
@@ -56,36 +80,21 @@ function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
 
 /** Parse the response from the fetch query into something we can display */
 async function parseFetchResponse(response: Response) {
-  const json = (await response.json()) as
-    | {
-        results: {
-          package: {
-            name: string;
-            description?: string;
-            publisher?: { username: string };
-            links: { npm: string };
-          };
-        }[];
-      }
-    | { code: string; message: string };
-
+  const json = (await response.json()) as { items: SearchResult[] } | { code: string; message: string };
   if (!response.ok || "message" in json) {
     throw new Error("message" in json ? json.message : response.statusText);
   }
-
-  return json.results.map((result) => {
-    return {
-      name: result.package.name,
-      description: result.package.description,
-      username: result.package.publisher?.username,
-      url: result.package.links.npm,
-    } as SearchResult;
-  });
+  return json.items;
 }
 
 interface SearchResult {
-  name: string;
-  description?: string;
-  username?: string;
+  uid: string;
+  title: string;
+  subtitle: string;
   url: string;
+  icon: string;
+  last_edited_at: string;
+  parent_url: string;
+  parent_path: string;
+  owledge_search_url: string;
 }
